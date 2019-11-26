27a4411ebd50b8392ed4df939b0a9e2dd4325953
mohammad alsboul <mohammad.alspoole@gmail.com>
2019-11-26 13:59:06 +0200

    lab12

--- README.md
+++ README.md
@@ -3,3 +3,5 @@ in this project i will use the chart and list showing after the counter of showe
 i use function algorthims for the pictuer to show randomly in the project 
 i use som style and background for rhe index and BusMall to make more good to view 
 the hole project in the BusMall and app.js while the app.js contain evry thing logical in my project 
+
+https://github.com/alsboul/BusMall
--- js/app.js
+++ js/app.js
@@ -27,7 +27,6 @@ var centerImage = document.querySelector('#centerImage');
 var rightImage = document.querySelector('#rightImage');
 var imagesSection = document.querySelector('#imagesSection');
 var rounds = 25;
-var voteChart = [];
 
 
 
@@ -58,48 +57,6 @@ function getUniqueIndex() {
   }
   return index;
 }
-var ctx = document.getElementById('myChart').getContext('2d');
-var myChart = new Chart(ctx, {
-
-  type: 'bar',
-  data: {
-    labels: names,
-    datasets: [
-      {
-        label: '# of Votes',
-        data: voteChart ,
-        backgroundColor: [
-          'rgba(255, 99, 132, 0.2)',
-          'rgba(54, 162, 235, 0.2)',
-          'rgba(255, 206, 86, 0.2)',
-          'rgba(75, 192, 192, 0.2)',
-          'rgba(153, 102, 255, 0.2)',
-          'rgba(255, 159, 64, 0.2)'
-        ],
-        borderColor: [
-          'rgba(255, 99, 132, 1)',
-          'rgba(54, 162, 235, 1)',
-          'rgba(255, 206, 86, 1)',
-          'rgba(75, 192, 192, 1)',
-          'rgba(153, 102, 255, 1)',
-          'rgba(255, 159, 64, 1)'
-        ],
-        borderWidth: 1,
-      }
-    ],
-  },
-  options: {
-    scales: {
-      yAxes: [
-        {
-          ticks: {
-            beginAtZero: true,
-          },
-        }
-      ],
-    },
-  },
-});
 
 function render() {
   var leftProduct = BusMall.all[getUniqueIndex()];
